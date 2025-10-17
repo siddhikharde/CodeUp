c89823c22eaa96bdcae67377eff0408f96ba466c
siddhikharde <siddhikharde2005@gmail.com>
2025-10-17 11:26:21 +0530

    sidebar added

---
+++ js/home.js
@@ -0,0 +1,14 @@
+const hideSidebar = () =>{
+   const sidebar= document.getElementById("sidebar-container");
+    sidebar.style.display="none";
+    const hamburger= document.querySelector(".hamburger-menu");
+    hamburger.style.opacity="1";
+
+}
+const showSidebar = () =>{
+    const sidebar= document.getElementById("sidebar-container");
+    const hamburger= document.querySelector(".hamburger-menu");
+    hamburger.style.opacity="0";
+     sidebar.style.display="block";
+   sidebar.style.opacity="1";
+}
