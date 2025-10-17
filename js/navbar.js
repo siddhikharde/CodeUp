d6137b87b81804283600a1a126942af6cc104c18
siddhikharde <siddhikharde2005@gmail.com>
2025-10-18 00:01:34 +0530

    navbar and footer added to course page

---
+++ js/navbar.js
@@ -0,0 +1,15 @@
+const hideSidebar = () =>{
+   const sidebar= document.getElementById("sidebar-container");
+    sidebar.style.display="none";
+    const hamburger= document.querySelector(".hamburger-menu");
+    hamburger.style.opacity="1";
+}
+const showSidebar = () =>{
+    const sidebar= document.getElementById("sidebar-container");
+    const hamburger= document.querySelector(".hamburger-menu");
+    hamburger.style.opacity="0";
+     sidebar.style.display="block";
+     sidebar.style.opacity="1";
+     sidebar.style.zIndex="1100";
+     
+}
